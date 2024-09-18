11f740cc31081ac23a1b7150aa043c93f88218a1
NickStomps <nick.stomps@student.graafschapcollege.nl>
2024-09-18 11:42:39 +0200

    Mockup gemaakt van de activiteiten detail pagina, heb de activiteiten controller verwijderd omdat hij niet was gemigrate naar de database en ook niet klopte

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('home');
 });
+Route::get('/activiteit', function () {
+    return view('activiteitendetails');
+});
